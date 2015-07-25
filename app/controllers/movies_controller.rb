0eba882352b21fbf75ad494b943c81867b47dbcf
shunie99 <astroboy1985_28@hotmail.com>
2015-07-25 04:52:12 +0000

    add_create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,31 @@ class MoviesController < ApplicationController
         @movies = Movie.all
    
     end
+    
+    def new
+        @movie = Movie.new
+    end
+    
+    def show
+    
+    end
+    
+    def create
+        @movie = Movie.new(movie_params)
+        if @movie.save
+            redirect_to movie_path(@movie)
+        else render 'new'
+        
+        end
+    end
+    
+    def movie_params
+        params.require(:movie).permit(:title, :comment, :ranking)
+    end
+    
+    def set_movie
+        @movie = Movie.find(params[:id])
+    end
 
 end
+
