091ec0baa980e3a15c94b00b357342f0918f5e31
shunie99 <astroboy1985_28@hotmail.com>
2015-07-25 04:07:57 +0000

    add_index

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,8 @@
+class MoviesController < ApplicationController
+ before_action :set_movie, only: [:show, :edit, :update, :destroy]
+    def index
+        @movies = Movie.all
+   
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720151131) do
+ActiveRecord::Schema.define(version: 20150725035027) do
+
+  create_table "movies", force: true do |t|
+    t.string   "title"
+    t.text     "comment"
+    t.integer  "ranking"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
